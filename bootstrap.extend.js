abff10f10a885daa30e4843f5607db4e67237e53
henrygotmojo <henrygotmojo@gmail.com>
2021-05-24 11:11:13 +0800

    extend bootstrap with multiple modals overlay

--- bootstrap.extend.js
+++ bootstrap.extend.js
@@ -1,3 +1,21 @@
+/*------------------------+
+| MULTIPLE MODALS OVERLAY |
++-------------------------+
+
+[Usage]
+Fix overlay order when multiple modals launched
+
+[Reference]
+https://stackoverflow.com/questions/19305821/multiple-modals-overlay
+*/
+$(document).on('show.bs.modal', '.modal', function (event) {
+	var zIndex = 1040 + (10 * $('.modal:visible').length);
+	$(this).css('z-index', zIndex);
+	setTimeout(function() {
+		$('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+	}, 0);
+});
+
 
 
 
